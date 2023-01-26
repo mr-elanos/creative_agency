3d60012861d5a4a7cce4e3c8f982d431d0a853be
Oleksii <moody2mood@gmail.com>
2023-01-26 20:18:06 +0200

    block skills

---
+++ static/js/main.js
@@ -0,0 +1,26 @@
+new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    breakpoints: {
+        320:{
+            slidesPerView: 1,
+        },
+        767:{
+            slidesPerView: 2,
+        },
+        1023:{
+            slidesPerView: 3,
+        },
+    },
+    loop: true,
+    speed: 500,
+    slidesPerView: 3,
+    spaceBetween: 25,
+    centeredSlides: true,
+    grabCursor: true,
+});
