a9c01dac8d11d2432725088d52c3f99ff1672a30
Oleksii <moody2mood@gmail.com>
2023-02-01 10:02:19 +0200

    start flying blocks

--- static/js/main.js
+++ static/js/main.js
@@ -41,3 +41,29 @@ $(function(){
         });
     });
 });
+
+const animItems = document.querySelectorAll('.anim-item');
+
+if (animItems.length > 0){
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll(params){
+        for (let index=0; index<animItems.length; index++){
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 3; // 1/3 of height element or window
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {animItemPoint = window.innerHeight - window.innerHeight / animStart};
+            if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset +animItemHeight)) {animItem.classList.add('show-anim')};
+        };
+    };
+};
+
+function offset(el){
+    const rect = el.getBoundingClientRect();
+    scrollLeft = window.scrollX || document.documentElement.scrollLeft;
+    scrollTop = window.scrollY || document.documentElement.scrollTop;
+    return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
+}   
+
